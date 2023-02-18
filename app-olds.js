c6846d730fd960a563652566070a831007a862bb
Walter <walteribanez555@gmail.com>
2023-02-17 20:30:09 -0400

    webserver ready

---
+++ app-olds.js
@@ -0,0 +1,19 @@
+
+const http = require('http');
+
+
+http.createServer( ( req, res)=> {
+    
+    
+    
+     
+   res.end();
+
+    
+
+
+})
+.listen( 8080 );
+
+console.log('Escuchando el puerto', 8080);
+
---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+require('dotenv').config()
+
+
+
+const hbs = require('hbs');
+
+
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine','hbs');
+
+hbs.registerPartials(__dirname+'/views/partials')
+
+//  Servir contenido estatico
+
+app.use( express.static('public')  );
+
+app.get('/', ( req, res)=>{
+   res.render('home', { 
+    nombre : 'Walter Ibanez',
+    titulo: 'Curso de Node'
+   });
+
+
+});
+
+
+app.get('/generic', ( req, res)=>{
+    res.render('generic', { 
+        nombre : 'Walter Ibanez',
+        titulo: 'Curso de Node'
+       });
+});
+
+app.get('/elements', ( req, res)=>{
+    res.render('elements',{ 
+        nombre : 'Walter Ibanez',
+        titulo: 'Curso de Node'
+       })
+});
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname + '/public/404.html');
+});
+
+
+app.listen(port, ()=> {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
